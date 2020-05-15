8d24156819f562f44b85ce65aa35046301b9c23a
Jonny Harte <jonathan.peter.harte@gmail.com>
2020-05-15 17:10:22 +0100

    Add GraphQL config & Linting, including pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    es2017: true,
+    es2020: true,
+    jest: true,
+  },
+  globals: { graphql: true, __PATH_PREFIX__: true },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+      modules: true,
+    },
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react-redux/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["emotion", "graphql", "prettier", "react-redux"],
+  rules: {
+    "emotion/jsx-import": "error",
+    "emotion/no-vanilla": "error",
+    "emotion/import-from-emotion": "error",
+    "emotion/styled-import": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "graphql/template-strings": [
+      `error`,
+      {
+        env: `relay`,
+        tagName: `graphql`,
+      },
+    ],
+  },
+}
