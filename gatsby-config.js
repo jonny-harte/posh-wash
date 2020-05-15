ea80e9703f0c684edacca082b48dd1966f8a77fb
Jonny Harte <jonathan.peter.harte@gmail.com>
2020-05-15 18:21:22 +0100

    Amend project metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Posh Wash`,
+    description: `London's number one hand car wash and alloy wheel refurbishment centre.`,
+    author: `Jonathan Harte`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Posh Wash`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
