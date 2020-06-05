a030d3679f8d6ad5c8522b604a0142705bedbf2a
Jonny Harte <jonathan.peter.harte@gmail.com>
2020-06-05 21:00:01 +0100

    Add getMenuItemByURL function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+export { default as getMenuItemByURL } from "./getMenuItemByURL/getMenuItemByURL"
 export * from "./isGooglePlaceOpen/isGooglePlaceOpen"
 export { default as telAlert } from "./telAlert/telAlert"
 export { default as fluidImageFragment } from "./fragments"
