eb327e18b0d390aa23b99fcb5220883ce48c9a96
Jonny Harte <jonathan.peter.harte@gmail.com>
2020-09-11 17:08:57 +0100

    Style 404 with Container component (#11)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 
+import Container from "../components/container/container.component"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <section>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          Take me{" "}
+          <Link href="/" title="Return to homepage">
+            home!
+          </Link>
+        </p>
+      </section>
+    </Container>
   </Layout>
 )
 
